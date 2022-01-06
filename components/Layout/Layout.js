a8bd80679c386571578b0e99175890b37fdbff43
l3n3mYl <asavkinas123@gmail.com>
2022-01-06 11:19:47 +0000

    Home page is now fully functional

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import styles from './Layout.module.scss'
 import Navbar from '../Navbar'
-import { node } from 'prop-types'
+import { node, array, string } from 'prop-types'
 
-const Layout = ({ children, refs }) => {
-
+const Layout = ({ children, refs, title }) => {
+  
   return (
     <div className={styles.Content}>
-      <Navbar refs={refs} />
+      <Navbar refs={refs} title={title} />
       {children}
     </div>
   )
 }
 
 Layout.propTypes = {
-  children: node
+  children: node,
+  refs: array.isRequired,
+  title: string.isRequired
 }
 
 export default Layout
