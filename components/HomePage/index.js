b11d2a2de8370b21fdf721572fc7d417df812829
l3n3mYl <asavkinas123@gmail.com>
2022-01-12 12:45:56 +0000

    Parallax is now added to home screen

--- components/HomePage/index.js
+++ components/HomePage/index.js
@@ -2,18 +2,21 @@ import React from 'react'
 import classNames from 'classnames'
 import { string, object } from 'prop-types'
 import styles from './HomePage.module.scss'
-import AnyImage from '../Handlers/ImageHandler'
+import Parallax from '../Animations/Parallax'
 
 
 const HomePage = ({ home, refer, id, className }) => {
 
   return (
     <div ref={refer} id={id} className={classNames(styles.HomePage, className)}>
-      <AnyImage className={styles.responsiveImage} src={home.image} />
-      <div className={styles.quote}>
+      <Parallax 
+        image={home.image} 
+        strength={250}
+      >
+        <div className={styles.quote}/>
         <p className={styles.title}>{home.title}</p>
         <p className={styles.subtitle}>{home.subtitle}</p>
-      </div>
+      </Parallax>
     </div>
   )
 }
